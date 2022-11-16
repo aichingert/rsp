64e8506ff2e9d747c16da292793f7e081405917e
Tobias <aichinger.tobias13@gmail.com>
2022-11-16 20:31:48 +0100

    list (stable): implemented list new and body

--- src/collections/linked_list.rs
+++ src/collections/linked_list.rs
@@ -1,8 +1,27 @@
 use std::fmt::Display;
 
-pub struct LinkedList {
-
+#[derive(Debug)]
+pub struct LinkedList<T: Display> {
+    head: Option<Node<T>>,
+    len: usize
 }
+
+impl<T: Display> LinkedList<T> {
+    pub fn new() -> Self {
+        Self {
+            head: None,
+            len: 0
+        }
+    }
+
+    pub fn from_nodes() -> Self {
+        Self {
+            head: None,
+            len: 0
+        }
+    }
+}
+
 
 #[derive(Debug)]
 pub struct Node<T: Display> {

0dec38ef87fb6d2f16d160e64301ba4c3d1de34d
Tobias <aichinger.tobias13@gmail.com>
2022-11-17 18:40:48 +0100

    tests (stable): added basic tests

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,12 +1,3 @@
 use rust_playground::collections::linked_list::*;
 
-fn main() {
-    let mut list: LinkedList<i32> = LinkedList::new();
-
-    list.push(10);
-    list.push(20);
-
-    println!("{:?}", list.pop());
-
-    println!("{:?}", list);
-}
+fn main() {}
--- src/collections/linked_list.rs
+++ src/collections/linked_list.rs
@@ -1,16 +1,16 @@
-use std::fmt::{self, Display, Debug};
+use std::fmt::Display;
 use std::mem;
 
 type Link<T> = Option<Box<Node<T>>>;
 
-#[derive(Debug)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct LinkedList<T: Display + Clone> {
     head: Link<T>
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 struct Node<T: Display + Clone> {
-    value: T,
+    elem: T,
     next: Link<T>
 }
 
@@ -21,7 +21,7 @@ impl<T: Display + Clone> LinkedList<T> {
 
     pub fn push(&mut self, elem: T) {
         let next: Box<Node<T>> = Box::new(Node {
-            value: elem,
+            elem,
             next: mem::replace(&mut self.head, None)
         });
 
@@ -33,9 +33,38 @@ impl<T: Display + Clone> LinkedList<T> {
             None => None,
             Some(node) => {
                 self.head = node.next;
-                Some(node.value)
+                Some(node.elem)
             }
         }
     }
 }
 
+impl<T: Display + Clone> Drop for LinkedList<T> {
+    fn drop(&mut self) {
+        let mut current: Link<T> = mem::replace(&mut self.head, None);
+        while let Some(mut node) = current {
+            current = mem::replace(&mut node.next, None);
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::{LinkedList, Node};
+    use crate::linked_list::Link;
+
+    #[test]
+    fn new_list_should_be_none() {
+        let list: LinkedList<i32> = LinkedList::new();
+        assert_eq!(list.head, None)
+    }
+
+    #[test]
+    fn push_test() {
+        let mut list: LinkedList<char> = LinkedList::new();
+        list.push('a');
+        list.push('b');
+
+        assert_eq!(list.pop(), Some('b'));
+    }
+}

ce4e9ae6350dfe3f99716d256e107c565d0a8f94
Tobias <aichinger.tobias13@gmail.com>
2022-11-17 18:49:22 +0100

    list (stable): added create from_elements function for list

--- src/collections/linked_list.rs
+++ src/collections/linked_list.rs
@@ -4,19 +4,29 @@ use std::mem;
 type Link<T> = Option<Box<Node<T>>>;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub struct LinkedList<T: Display + Clone> {
+pub struct LinkedList<T: Display + Clone + Copy> {
     head: Link<T>
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
-struct Node<T: Display + Clone> {
+struct Node<T: Display + Clone + Copy> {
     elem: T,
     next: Link<T>
 }
 
-impl<T: Display + Clone> LinkedList<T> {
+impl<T: Display + Clone + Copy> LinkedList<T> {
     pub fn new() -> Self {
         Self { head: None }
+    }
+
+    pub fn from_elements(elements: &[T]) -> Self {
+        let mut list: LinkedList<T> = LinkedList::new();
+
+        for i in 0..elements.len() {
+            list.push(elements[elements.len() - i - 1]);
+        }
+
+        list
     }
 
     pub fn push(&mut self, elem: T) {
@@ -39,7 +49,7 @@ impl<T: Display + Clone> LinkedList<T> {
     }
 }
 
-impl<T: Display + Clone> Drop for LinkedList<T> {
+impl<T: Display + Clone + Copy> Drop for LinkedList<T> {
     fn drop(&mut self) {
         let mut current: Link<T> = mem::replace(&mut self.head, None);
         while let Some(mut node) = current {
@@ -66,5 +76,17 @@ mod test {
         list.push('b');
 
         assert_eq!(list.pop(), Some('b'));
+    }
+
+    #[test]
+    fn create_list_from_chars() {
+        let elements: [char; 3] = ['a','b','c'];
+        let mut list: LinkedList<char> = LinkedList::from_elements(&elements);
+        let mut idx: usize = 0;
+
+        while let Some(elem) = list.pop() {
+            assert_eq!(elem, elements[idx]);
+            idx+=1;
+        }
     }
 }

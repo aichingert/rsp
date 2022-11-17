092a4247be5c2284e294a2ac6fea93bb796b951d
Tobias <aichinger.tobias13@gmail.com>
2022-11-17 18:20:21 +0100

    List (stable): added generics

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,7 +1,7 @@
 use rust_playground::collections::linked_list::*;
 
 fn main() {
-    let mut list: LinkedList = LinkedList::new();
+    let mut list: LinkedList<i32> = LinkedList::new();
 
     list.push(10);
     list.push(20);
--- src/collections/linked_list.rs
+++ src/collections/linked_list.rs
@@ -1,26 +1,26 @@
 use std::fmt::{self, Display, Debug};
 use std::mem;
 
-type Link = Option<Box<Node>>;
+type Link<T> = Option<Box<Node<T>>>;
 
 #[derive(Debug)]
-pub struct LinkedList {
-    head: Link
+pub struct LinkedList<T: Display + Clone> {
+    head: Link<T>
 }
 
 #[derive(Debug)]
-struct Node {
-    value: i32,
-    next: Link
+struct Node<T: Display + Clone> {
+    value: T,
+    next: Link<T>
 }
 
-impl LinkedList {
+impl<T: Display + Clone> LinkedList<T> {
     pub fn new() -> Self {
         Self { head: None }
     }
 
-    pub fn push(&mut self, elem: i32) {
-        let next: Box<Node> = Box::new(Node {
+    pub fn push(&mut self, elem: T) {
+        let next: Box<Node<T>> = Box::new(Node {
             value: elem,
             next: mem::replace(&mut self.head, None)
         });
@@ -28,7 +28,7 @@ impl LinkedList {
         self.head = Some(next);
     }
 
-    pub fn pop(&mut self) -> Option<i32> {
+    pub fn pop(&mut self) -> Option<T> {
         match mem::replace(&mut self.head, None) {
             None => None,
             Some(node) => {

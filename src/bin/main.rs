b55f460a7c916bf75739b84121da832970ec193c
Tobias <aichinger.tobias13@gmail.com>
2022-11-17 17:55:57 +0100

    List design (stable): changed list design again

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,10 +1,7 @@
 use rust_playground::collections::linked_list::*;
 
 fn main() {
-    let mut node: LinkedList<i32> = LinkedList::new(10);
-    node.push(10);
-    node.push(20);
-    node.push(10);
+    let mut node: LinkedList = LinkedList::new();
 
-    println!("{}", node);
+    println!("{:?}", node);
 }
--- src/collections/linked_list.rs
+++ src/collections/linked_list.rs
@@ -1,53 +1,28 @@
 use std::fmt::{self, Display, Debug};
 
-#[derive(Debug, Clone)]
-pub struct LinkedList<T: Display + Debug> {
-    value: T,
-    len: usize,
-    next: Node<T>
+#[derive(Debug)]
+pub struct LinkedList {
+    head: Link
 }
 
-impl<T: Display + Debug> LinkedList<T> {
-    pub fn new(value: T) -> Self {
+#[derive(Debug)]
+enum Link {
+    Next(Box<Node>),
+    Null 
+}
+
+#[derive(Debug)]
+struct Node {
+    value: i32,
+    next: Link
+}
+
+impl LinkedList {
+    pub fn new() -> Self {
         Self {
-            value,
-            len: 1,
-            next: Node::nil
-        }
-    }
-
-    pub fn push(&mut self, value: T) {
-        match self.next {
-            Node::node(ref mut next_node) => {
-                next_node.push(value);
-            },
-            Node::nil => {
-                let node = LinkedList::new(value);
-                self.next = Node::node(Box::new(node));
-            }
+            head: Link::Null
         }
     }
 }
 
-impl<T: Display + Debug> Display for LinkedList<T> {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self.next {
-            Node::node(ref next_node) => {
-                println!("Node:{}", self.value);
-                std::fmt::Display::fmt(&next_node, f)
-            },
-            Node::nil => {
-                Ok(())
-            }
-        }
-    }
-}
-
-
-
-#[derive(Debug, Clone)]
-pub enum Node<T: Display + Debug> {
-    node(Box::<LinkedList<T>>),
-    nil
-}
 
